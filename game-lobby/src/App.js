c479347dd817dc1929507d5df96a77fe8ceb3ca1
Edward Cao <yifan.cao@uoit.net>
2022-04-30 18:38:58 -0400

    + React Framework & MaterialUI

---
+++ game-lobby/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    {/* Lobby Title */}
+    <h1>Game Lobby</h1>
+    {/* Player Boxes */}
+    </div>
+  );
+}
+
+export default App;
